57214c09ef165a47b7264dfa382d10dd5e8d7a4e
jetty8013 <jetty8013@gmail.com>
2023-02-17 12:50:58 +0900

    Chore : eslint,prettier 설정

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,9 @@
 /** @type {import('tailwindcss').Config} */
+/* eslint-disable */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};

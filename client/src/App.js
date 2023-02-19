80d45dd43426ccbdcd153c7c58da38029ad4dbab
Jaehun Kim <dev4jaehun.kim@gmail.com>
2023-02-19 13:20:11 +0900

    feat: add router

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import Header from './components/Header';
-import Login from './components/Login';
+import Login from './pages/Login';
+import Home from './pages/Home';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="flex flex-wrap flex-row justify-center items-start w-[100%]] h-[100vh]">
       <Header />
-      <Login />
+      <Routes>
+        <Route exact path="/seb42_pre_034/" element={<Home />} />
+        <Route path="/seb42_pre_034/login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
---
+++ client/src/Pages/Home.js
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div>
+      <h1>This is Home page</h1>
+    </div>
+  );
+}
+
+export default Home;

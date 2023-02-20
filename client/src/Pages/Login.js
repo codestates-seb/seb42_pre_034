fa05b86988b7a50f491cf4311d481c2235e3ee0d
jetty8013 <jetty8013@gmail.com>
2023-02-20 12:48:56 +0900

    design : Login 페이지 css 수정

--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -1,41 +1,16 @@
-import iconNaver from '../assets/icon_naver.png';
+import ic_naver from '../assets/icon_naver.png';
+import { ReactComponent as IC_kakao } from '../assets/ic_kakao.svg';
+import { ReactComponent as IC_google } from '../assets/ic_google.svg';
+import { ReactComponent as IC_stackoverflow } from '../assets/ic_stackoverflow.svg';
+import { ReactComponent as IC_share } from '../assets/ic_share.svg';
 
 const Login = () => {
   return (
-    <div className="w-[288.45px] h-[555.2px] relative">
-      <div className="flex justify-center items-start w-[288.45px] h-[61px] absolute left-0 top-0 overflow-hidden">
-        <svg
-          width={33}
-          height={37}
-          viewBox="0 0 33 37"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-          className="flex-grow-0 flex-shrink-0 w-8 h-[37px] relative"
-          preserveAspectRatio="none"
-        >
-          <g clipPath="url(#clip0_34_272)">
-            <path
-              d="M26.225 33V24H30.225V37H0.224976V24H4.22498V33H26.225Z"
-              fill="#BCBBBB"
-            />
-            <path
-              d="M21.725 0L19.025 2L28.925 15.3L31.625 13.3L21.725 0ZM26.225 18.4L13.525 7.8L15.625 5.3L28.325 15.9L26.225 18.4ZM9.32498 15.2L24.325 22.2L25.725 19.2L10.725 12.2L9.32498 15.2ZM23.325 25.99L24.005 23.04L7.90498 19.69L7.22498 23L23.325 25.99ZM23.225 30H7.22498V27H23.225V30Z"
-              fill="#601BB3"
-            />
-          </g>
-          <defs>
-            <clippath id="clip0_34_272">
-              <rect
-                width={32}
-                height={37}
-                fill="white"
-                transform="translate(0.224976)"
-              />
-            </clippath>
-          </defs>
-        </svg>
+    <div className="w-[288.45px] h-[555.2px] relative select-none">
+      <div className="flex justify-center items-start w-[288.45px] h-[61px] absolute left-0 top-0">
+        <IC_stackoverflow />
       </div>
-      <div className="flex justify-center items-center w-[288.45px] h-[102px] absolute left-0 top-[453.2px] overflow-hidden p-4">
+      <div className="flex justify-center items-center w-[288.45px] h-[102px] absolute left-0 top-[453.2px] p-4">
         <div className="flex-grow-0 flex-shrink-0 w-[261px] h-8">
           <p className="absolute left-[13.73px] top-[35px] text-[13px] text-center">
             <span className="text-[13px] text-center text-black">
@@ -54,90 +29,25 @@ const Login = () => {
               Sign up on Talent
             </span>
           </p>
-          <svg
-            width={15}
-            height={15}
-            viewBox="0 0 15 15"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            className="absolute left-[260.23px] top-[52.5px]"
-            preserveAspectRatio="none"
-          >
-            <path
-              d="M5.39176 0.199951H3.05843C2.43959 0.199951 1.8461 0.445784 1.40852 0.883369C0.97093 1.32095 0.725098 1.91445 0.725098 2.53328V11.8666C0.725098 13.15 1.7751 14.2 3.05843 14.2H12.3918C13.0106 14.2 13.6041 13.9541 14.0417 13.5165C14.4793 13.0789 14.7251 12.4855 14.7251 11.8666V9.53328H12.3918V11.8666H3.05843V2.53328H5.39176V0.199951ZM7.7251 0.199951H14.7251V7.19995H12.3918V4.28328L7.14176 9.53328L5.39176 7.78328L10.6418 2.53328H7.7251V0.199951Z"
-              fill="#0075CF"
-            />
-          </svg>
+          <IC_share className="absolute left-[255.23px] top-[57.5px]"/>
         </div>
       </div>
-      <div className="flex flex-col justify-start items-start w-[288.45px] h-[158px] absolute left-[0.23px] top-[61px] overflow-hidden gap-1 py-1">
-        <div className="flex justify-start items-center flex-grow-0 flex-shrink-0 w-[288.45px] h-[37.8px] relative overflow-hidden gap-[7px] px-[82px] py-[9px] rounded-[5px] bg-white border-[0.5px] border-[#9fa6ad]">
-          <svg
-            width={19}
-            height={19}
-            viewBox="0 0 19 19"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            className="flex-grow-0 flex-shrink-0 w-[18px] h-[18px] relative"
-            preserveAspectRatio="none"
-          >
-            <path
-              d="M16.7351 8.90001H9.20508V11.9H13.5051C13.3251 12.9 12.7651 13.38 11.9051 13.94V15.95H14.5051C15.2915 15.1913 15.9098 14.2758 16.3198 13.2629C16.7298 12.2499 16.9224 11.1622 16.8851 10.07C16.8851 9.50001 16.8351 9.41001 16.7351 8.89001V8.90001Z"
-              fill="#4285F4"
-            />
-            <path
-              d="M9.20518 17.9C11.3652 17.9 13.1752 17.18 14.5052 15.96L11.9052 13.96C11.2878 14.3612 10.5865 14.615 9.85537 14.7017C9.12426 14.7885 8.38299 14.7059 7.6889 14.4604C6.99481 14.2148 6.36651 13.8129 5.8526 13.2857C5.33868 12.7585 4.95293 12.1202 4.72518 11.42H2.05518V13.49C2.72089 14.8158 3.74212 15.9303 5.00475 16.7091C6.26738 17.4878 7.72169 17.9002 9.20518 17.9Z"
-              fill="#34A853"
-            />
-            <path
-              d="M4.72509 11.42C4.39572 10.4334 4.39572 9.36657 4.72509 8.38V6.31H2.05509C1.49567 7.42404 1.20435 8.65339 1.20435 9.9C1.20435 11.1466 1.49567 12.376 2.05509 13.49L4.72509 11.42Z"
-              fill="#FBBC05"
-            />
-            <path
-              d="M9.20518 5.08004C10.3752 5.08004 11.4352 5.48004 12.2652 6.28004L14.5652 3.98004C13.6526 3.1535 12.5616 2.54875 11.3771 2.21287C10.1926 1.87698 8.94651 1.81904 7.73593 2.04354C6.52535 2.26805 5.38292 2.76894 4.3976 3.50722C3.41229 4.24551 2.61067 5.20125 2.05518 6.30004L4.72518 8.39004C5.03273 7.44039 5.63045 6.61115 6.43416 6.01914C7.23786 5.42712 8.20704 5.10217 9.20518 5.09004V5.08004Z"
-              fill="#EA4335"
-            />
-          </svg>
+      <div className="flex flex-col justify-start items-start w-[288.45px] h-[158px] absolute left-[0.23px] top-[61px] gap-1 py-1">
+        <div className="cursor-pointer flex justify-start items-center flex-grow-0 flex-shrink-0 w-[288.45px] h-[37.8px] gap-[7px] px-[82px] py-[9px] rounded-[5px] bg-white border-[0.5px] border-[#9fa6ad]">
+          <IC_google />
           <p className="flex-grow-0 flex-shrink-0 text-[13px] font-medium text-center text-[#3b4045]">
             Log in with Google
           </p>
         </div>
-        <div className="flex justify-start items-center flex-grow-0 flex-shrink-0 w-[288.45px] h-[37.8px] relative overflow-hidden gap-[7px] px-[82px] py-[9px] rounded-[5px] bg-[#fee500] border-[0.5px] border-[#9fa6ad]">
-          <svg
-            width={19}
-            height={17}
-            viewBox="0 0 19 17"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            className="flex-grow-0 flex-shrink-0 w-[18px] h-4 relative"
-            preserveAspectRatio="none"
-          >
-            <g clipPath="url(#clip0_34_276)">
-              <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M8.81144 14.3067C13.5536 14.3067 17.3978 11.2657 17.3978 7.51453C17.3978 3.76331 13.5536 0.722351 8.81144 0.722351C4.06934 0.722351 0.225098 3.76331 0.225098 7.51453C0.225098 9.92918 1.81794 12.0495 4.21779 13.254L3.30075 16.6776L7.01932 14.1585C7.59734 14.2556 8.19685 14.3067 8.81144 14.3067Z"
-                fill="#3B1E1E"
-              />
-            </g>
-            <defs>
-              <clippath id="clip0_34_276">
-                <rect
-                  width={18}
-                  height={16}
-                  fill="white"
-                  transform="translate(0.225098 0.700012)"
-                />
-              </clippath>
-            </defs>
-          </svg>
+        <div className="cursor-pointer flex justify-start items-center flex-grow-0 flex-shrink-0 w-[288.45px] h-[37.8px] gap-[7px] px-[82px] py-[9px] rounded-[5px] bg-[#fee500] border-[0.5px] border-[#9fa6ad]">
+          <IC_kakao />
           <p className="flex-grow-0 flex-shrink-0 text-[13px] font-medium text-center text-[#3b4045]">
             Log in with KaKao
           </p>
         </div>
-        <div className="flex justify-start items-center flex-grow-0 flex-shrink-0 w-[288.45px] h-[37.8px] relative gap-[7px] px-[82px] py-[9px] rounded-[5px] bg-[#22c75a] border-[0.5px] border-[#9fa6ad]">
+        <div className="cursor-pointer flex justify-start items-center flex-grow-0 flex-shrink-0 w-[288.45px] h-[37.8px] gap-[7px] px-[82px] py-[9px] rounded-[5px] bg-[#22c75a] border-[0.5px] border-[#9fa6ad]">
           <img
-            src={iconNaver}
+            src={ic_naver}
             className="flex-grow-0 flex-shrink-0 w-[18px] h-[16.2px] object-cover"
             alt=""
           />
@@ -147,7 +57,7 @@ const Login = () => {
         </div>
       </div>
       <div
-        className="flex flex-col justify-start items-center w-[288.45px] h-[234.2px] absolute left-0 top-[219px] overflow-hidden gap-1.5 p-6 rounded-[7px] bg-white"
+        className="flex flex-col justify-start items-center w-[288.45px] h-[234.2px] absolute left-0 top-[219px] gap-1.5 p-6 rounded-[7px] bg-white"
         style={{ boxShadow: '2px 2px 4px 0 rgba(0,0,0,0.25)' }}
       >
         <div className="flex justify-start items-center flex-grow-0 flex-shrink-0 w-[240.45px] relative py-1.5">
@@ -155,7 +65,7 @@ const Login = () => {
             Email
           </p>
         </div>
-        <div className="flex-grow-0 flex-shrink-0 w-[240.45px] h-[32.59px] relative overflow-hidden rounded-[3px] bg-white border-[0.5px] border-[#9fa6ad]" />
+        <input className="flex-grow-0 flex-shrink-0 w-[240.45px] h-[32.59px] pl-2 relative rounded-[3px] bg-white border-[0.5px] border-[#9fa6ad]" />
         <div className="flex justify-between items-center flex-grow-0 flex-shrink-0 w-[240.45px] relative py-1.5">
           <p className="flex-grow-0 flex-shrink-0 text-[15px] font-semibold text-center text-black">
             Password
@@ -164,8 +74,8 @@ const Login = () => {
             Forgot password?
           </p>
         </div>
-        <div className="flex-grow-0 flex-shrink-0 w-[240.45px] h-[32.59px] relative overflow-hidden rounded-[3px] bg-white border-[0.5px] border-[#9fa6ad]" />
-        <div className="flex justify-center items-center flex-grow-0 flex-shrink-0 w-[240.45px] h-[37.8px] relative overflow-hidden gap-2.5 p-2.5 rounded-[3px] bg-[#0a95ff]">
+        <input className="flex-grow-0 flex-shrink-0 w-[240.45px] h-[32.59px] pl-2 relative rounded-[3px] bg-white border-[0.5px] border-[#9fa6ad] " />
+        <div className="cursor-pointer flex justify-center items-center flex-grow-0 flex-shrink-0 w-[240.45px] h-[37.8px] gap-2.5 p-2.5 rounded-[3px] bg-[#0a95ff] hover:bg-[#0074cc]">
           <p className="flex-grow-0 flex-shrink-0 w-[114px] text-[13px] font-medium text-center text-white">
             Log in
           </p>

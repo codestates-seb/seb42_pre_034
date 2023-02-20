2bbaf6e4686a85abc5c6aabe3e38226b919da9bc
Jaehun Kim <dev4jaehun.kim@gmail.com>
2023-02-20 20:50:16 +0900

    feat: add dummy data for render Questions page

--- client/src/Pages/Questions.js
+++ client/src/Pages/Questions.js
@@ -1,7 +1,25 @@
 function Questions() {
+  const questions = [
+    {
+      title: "Dummy Question 1",
+      body: "Dummy Question 1's body",
+      vote: 1,
+      answer: 3,
+      view: 5,
+      tag: ["dummy", "fake"]
+    },
+    {
+      title: "How to write better code than ChatGPT",
+      body: "I want to be a very valuable front-end engineer. How do you do that? I think about it. If I can write better code than ChatGPT, will I become a very valuable front-end engineer?",
+      vote: -1,
+      answer: 0,
+      view: 2,
+      tag: ["career", "chatgpt", "ai"]
+    }
+  ];
   return (
     <>
-      <h1>This is Questions page</h1>
+      <h1>All Questions</h1>
     </>
   )
 }

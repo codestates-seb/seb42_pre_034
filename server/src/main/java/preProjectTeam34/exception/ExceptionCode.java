ebc7db71095046680c04e7a95be938942075487c
Hyunsoo Lee <87977679+p1Zzal@users.noreply.github.com>
2023-02-21 09:26:26 +0900

    Add files via upload

---
+++ server/src/main/java/preProjectTeam34/exception/ExceptionCode.java
@@ -0,0 +1,19 @@
+package preProjectTeam34.exception;
+
+import lombok.Getter;
+
+public enum ExceptionCode {
+
+    QUESTION_NOT_FOUND(404, "Question Not Found"),
+    QUESTION_DELETE(409, "Question Delete");
+
+    @Getter
+    private int status;
+    @Getter
+    private String message;
+
+    ExceptionCode(int code, String message){
+        this.status = code;
+        this.message = message;
+    }
+}
---
+++ server/src/main/java/preProjectTeam34/question/service/QuestionService.java
@@ -0,0 +1,71 @@
+package preProjectTeam34.question.service;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import preProjectTeam34.exception.BusinessLogicException;
+import preProjectTeam34.exception.ExceptionCode;
+import preProjectTeam34.question.entity.Question;
+import preProjectTeam34.question.repository.QuestionRepository;
+
+import java.util.Optional;
+
+@Transactional
+@Service
+public class QuestionService {
+    private final QuestionRepository questionRepository;
+
+    public QuestionService(QuestionRepository questionRepository) {
+        this.questionRepository = questionRepository;
+    }
+
+    public Question createQuestion(Question question){
+        Question saveQuestion = questionRepository.save(question);
+
+        return saveQuestion;
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.SERIALIZABLE)
+    public Question updateQuestion(Question question){
+        Question findQuestion = findVerifiedQuestion(question.getQuestionId());
+
+        Optional.ofNullable(question.getContent())
+                .ifPresent(content -> findQuestion.setContent(content));
+        Optional.ofNullable(question.getTitle())
+                .ifPresent(title -> findQuestion.setTitle(title));
+        Optional.ofNullable(question.getQuestionStatus())
+                .ifPresent(questionStatus -> findQuestion.setQuestionStatus(questionStatus));
+
+        return questionRepository.save(findQuestion);
+    }
+
+    @Transactional(readOnly = true)
+    public Question findQuestion(long questionId){
+        return findVerifiedQuestion(questionId);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<Question> findQuestions(int page, int size){
+        return questionRepository.findAll(PageRequest
+                .of(page, size, Sort.by("questionId").descending()));
+    }
+
+    public void deleteQuestion(long questionId){
+        Question findQuestion = findVerifiedQuestion(questionId);
+
+        questionRepository.delete(findQuestion);
+    }
+
+    public Question findVerifiedQuestion(Long questionId){
+        Optional<Question> optionalQuestion = questionRepository.findByQuestion(questionId);
+
+        Question findQuestion =
+                optionalQuestion.orElseThrow(() -> new BusinessLogicException(ExceptionCode.QUESTION_NOT_FOUND));
+
+        return findQuestion;
+    }
+}

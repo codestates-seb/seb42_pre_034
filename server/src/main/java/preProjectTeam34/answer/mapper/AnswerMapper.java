c468ca815629278deee2b546d610850521536658
cStatesHwang <jjmes1992@gmail.com>
2023-02-23 08:58:49 +0900

    answer 수정 02.

--- server/src/main/java/preProjectTeam34/answer/mapper/AnswerMapper.java
+++ server/src/main/java/preProjectTeam34/answer/mapper/AnswerMapper.java
@@ -1,6 +1,7 @@
 package preProjectTeam34.answer.mapper;
 
 
+import org.mapstruct.Mapper;
 import preProjectTeam34.answer.dto.AnswerPatchDto;
 import preProjectTeam34.answer.dto.AnswerPostDto;
 import preProjectTeam34.answer.dto.AnswerResponseDto;
@@ -8,6 +9,7 @@ import preProjectTeam34.answer.entity.Answer;
 
 import java.util.List;
 
+@Mapper(componentModel = "spring")
 public interface AnswerMapper {
     Answer answerPostDtoToAnswer(AnswerPostDto answerPostDto);
     Answer answerPatchDtoToAnswer(AnswerPatchDto answerPatchDto);

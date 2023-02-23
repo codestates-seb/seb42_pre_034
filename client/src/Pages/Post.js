3096b0566a4f025fdbe79522d7a94bbd56c90e43
Jaehun Kim <dev4jaehun.kim@gmail.com>
2023-02-23 15:34:24 +0900

    Implement header section for <Post>

--- client/src/Pages/Post.js
+++ client/src/Pages/Post.js
@@ -7,11 +7,39 @@ import StackButton from '../components/StackButton';
 
 function Post() {
   return (
-    <>
-      This post id is 
-      <StackButton label="Ask Question" />
-      <StackButton label="Post Your Answer" />
-    </>
+    <div className='flex flex-col w-full'>
+      {/* Post header */}
+      <header className='flex flex-col pl-8'>
+        <div className='flex justify-between w-11/12 my-4'>
+          {/* TODO: 제목 부분을 받아온 데이터로 표시하도록 수정 */}
+          <h1 className='text-2xl'>How to write better code than ChatGPT</h1>
+          <StackButton label="Ask Question"/>
+        </div>
+        <div className='flex items-center gap-4 '>
+          <div className='flex justify-center items-center text-sm'>
+            <p className='text-[#8B9197] mr-1'>Asked</p>
+            {/* TODO: 서버에서 받아온 날짜로부터 계산된 값을 표시하기 */}
+            <p>Today</p>
+          </div>
+          <div className='flex justify-center items-center text-sm'>
+            <p className='text-[#8B9197] mr-1'>Modified</p>
+            {/* TODO: 서버에서 받아온 날짜로부터 계산된 값을 표시하기 */}
+            <p>Today</p>
+          </div>
+          <div className='flex justify-center items-center text-sm'>
+            <p className='text-[#8B9197] mr-1'>Viewed</p>
+            {/* TODO: 숫자 부분만 서버에서 받아온 조회수로 표시하기 */}
+            <p>18 times</p>
+          </div>
+        </div>
+        
+      </header>
+      <article className='flex flex-col'>
+        This post id is 
+        <StackButton label="Post Your Answer" />
+      </article>
+      
+    </div>
   )
 }
 
--- client/src/components/StackButton.js
+++ client/src/components/StackButton.js
@@ -1,6 +1,6 @@
 function StackButton({ label }) {
   return (
-    <div className="float-right w-fit px-2 h-10 relative overflow-hidden gap-2.5 py-2.5 rounded-[10px] bg-[#601bb3]">
+    <div className="w-fit px-2 h-10 relative overflow-hidden gap-2.5 py-2.5 rounded-[10px] bg-[#601bb3]">
       <p className="text-[13px] font-bold text-center text-white">
         {label}
       </p>

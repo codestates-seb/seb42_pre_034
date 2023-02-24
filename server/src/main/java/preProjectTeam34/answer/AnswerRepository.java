04a0593fa040e19355b0b226d1e6ed68fda2b9f2
TaeheeWoo94 <wth0086@gmail.com>
2023-02-24 10:55:08 +0900

    main merge 오류 해결

--- server/src/main/java/preProjectTeam34/answer/AnswerRepository.java
+++
@@ -1,9 +0,0 @@
-package preProjectTeam34.answer;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface AnswerRepository extends JpaRepository<Answer, Long> {
-
-}
